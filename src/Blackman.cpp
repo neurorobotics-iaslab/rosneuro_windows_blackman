797b5c612ae921300f884e7e7cf90cd0f4e31d7e
mirovix <miroljubmihailovic98@gmail.com>
2024-07-27 16:20:33 +0200

    feat: creating dependency on window

--- src/Blackman.cpp
+++ src/Blackman.cpp
@@ -1,6 +1,6 @@
 #include "rosneuro_windows_blackman/Blackman.hpp"
 #include "pluginlib/class_list_macros.h"
 
-PLUGINLIB_EXPORT_CLASS(rosneuro::Blackman<int>,    rosneuro::Filter<int>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::Blackman<float>,  rosneuro::Filter<float>)
-PLUGINLIB_EXPORT_CLASS(rosneuro::Blackman<double>, rosneuro::Filter<double>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Blackman<int>,    rosneuro::Window<int>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Blackman<float>,  rosneuro::Window<float>)
+PLUGINLIB_EXPORT_CLASS(rosneuro::Blackman<double>, rosneuro::Window<double>)
